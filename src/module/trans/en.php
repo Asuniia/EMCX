f6792033ba5aa25b527db7605b4e610dd2895444
Asuniia <43389096+Asuniia@users.noreply.github.com>
2022-05-02 21:36:34 +0200

    Update: Trans file server list

--- src/module/trans/en.php
+++ src/module/trans/en.php
@@ -8,7 +8,18 @@ return [
             "change_repo" => "Change server",
             "enabled" => "Active",
             "disabled" => "Not active",
-            "update" => "Update"
+            "update" => "Update",
+            "servers" => [
+                "title" => "EMCX Servers",
+                "subtitle" => "List of activated and online EMCX servers.",
+                "connected_to" => "Connected to the server ",
+                "server_info" => "Server information",
+                "server_module" => "Online modules",
+                "server_join" => "Join server",
+                "server_already_join" => "You are already connected",
+                "no_online_server" => "No online server"
+            ]
+
         ],
 
     ],
--- src/module/trans/fr.php
+++ src/module/trans/fr.php
@@ -12,6 +12,12 @@ return [
             "servers" => [
                 "title" => "Serveurs EMCX",
                 "subtitle" => "Liste des serveurs EMCX activé et en ligne.",
+                "connected_to" => "Connecté sur le serveur ",
+                "server_info" => "Informations du serveur",
+                "server_module" => "Modules en ligne",
+                "server_join" => "Rejoindre le serveur",
+                "server_already_join" => "Vous êtes déjà connecté",
+                "no_online_server" => "Aucun serveur en ligne"
             ]
         ],
 
