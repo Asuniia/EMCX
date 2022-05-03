0c987db601a2a2cd16c4c988d8f86124a28b90c3
Asuniia <43389096+Asuniia@users.noreply.github.com>
2022-05-03 15:18:13 +0200

    Update: Trans

--- src/module/trans/en.php
+++ src/module/trans/en.php
@@ -6,6 +6,8 @@ return [
             "title" => "EMCX",
             "subtitle" => "EMCX Allows you to add unofficial modules to ClientX. Customize your choices with various online directories!",
             "change_repo" => "Change server",
+            "back_home" => "Back to home",
+            "welcome" => "Welcome to EMCX !",
             "enabled" => "Active",
             "disabled" => "Not active",
             "update" => "Update",
@@ -18,9 +20,31 @@ return [
                 "server_join" => "Join server",
                 "server_logout" => "Logout",
                 "server_already_join" => "You are already connected",
-                "no_online_server" => "No online server"
+                "no_online_server" => "No online server",
+            ],
+            "error" => "Error",
+            "retry" => "Retry",
+            "welcome_text" => "Discover a product catalog on an alternative store!",
+            "installed" => "Installed",
+            "online" => "Online",
+            "buy" => "Buy",
+            "download" => "Download",
+            "about" => "About",
+            "author" => "Author",
+            "updated" => "Update",
+            "version" => "Version",
+            "price" => "Price",
+            "free" => "Free",
+            "disable" => "Disable",
+            "enable" => "Enable",
+            "unknown_selected_server" => [
+                "unknown_server" => "No server selected",
+                "unknown_content" => "In order to use EMCX, please select a server from the list of servers.",
+            ],
+            "error_selected_server" => [
+                "error_server" => "Cannot reach the server",
+                "error_content" => "Please change the server or try again later. If the error persists, contact the server administrator.",
             ]
-
         ],
 
     ],
--- src/module/trans/fr.php
+++ src/module/trans/fr.php
@@ -6,6 +6,8 @@ return [
             "title" => "EMCX",
             "subtitle" => "EMCX Permet d'ajouter des modules non officiels sur ClientX. Personnalisez vos choix avec des divers répertoire en ligne!",
             "change_repo" => "Changer de serveur",
+            "back_home" => "Revenir à l'accueil",
+            "welcome" => "Bienvenue sur EMCX !",
             "enabled" => "Actif",
             "disabled" => "Non actif",
             "update" => "Mettre à jour",
@@ -19,8 +21,30 @@ return [
                 "server_logout" => "Déconnexion",
                 "server_already_join" => "Vous êtes déjà connecté",
                 "no_online_server" => "Aucun serveur en ligne"
+            ],
+            "error" => "Erreur",
+            "retry" => "Réessayer",
+            "welcome_text" => "Découvrez un catalogue de produits sur un magasin alternatif!",
+            "installed" => "Installés",
+            "online" => "En ligne",
+            "buy" => "Acheter",
+            "download" => "Télécharger",
+            "about" => "A propos",
+            "author" => "Auteur",
+            "updated" => "Mis à jour",
+            "version" => "Version",
+            "price" => "Prix",
+            "free" => "Gratuit",
+            "disable" => "Désactiver",
+            "enable" => "Activer",
+            "unknown_selected_server" => [
+                "unknown_server" => "Aucun serveur sélectionné",
+                "unknown_content" => "Pour pouvoir utiliser EMCX, veuillez sélectionner un serveur dans la liste des serveurs.",
+            ],
+            "error_selected_server" => [
+                "error_server" => "Impossible de joindre le serveur",
+                "error_content" => "Veuillez changer de serveur ou réessayer plus tard. Si l'erreur persiste, contacter l'administrateur du serveur.",
             ]
         ],
-
     ],
 ];
