0928740edd69847371c940fe8c092648ad8c9543
Asuniia <43389096+Asuniia@users.noreply.github.com>
2022-05-06 21:17:35 +0200

    Update Configuration.php

--- src/config/Configuration.php
+++ src/config/Configuration.php
@@ -22,15 +22,26 @@ class Configuration
         return $this->config;
     }
 
-    public function set($array, $keys)
+    public function setConfig($array, $keys)
     {
         $this->config[$array] = $keys;
-        $this->save($this->config);
+        $this->saveConfig($this->config);
     }
 
-    public function save(array $config)
+    public function saveConfig(array $config)
     {
         file_put_contents(dirname(__DIR__, 2) . self::SETTINGS_FILE, json_encode($config, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+    }
+
+    public function setItem($item,$array, $keys)
+    {
+        $this->items[$item][$array] = $keys;
+        $this->saveItem($this->items);
+    }
+
+    public function saveItem(array $items)
+    {
+        file_put_contents(dirname(__DIR__, 2) . self::MODULES_FILE, json_encode($items, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
     }
 
     public function getItems(): array
