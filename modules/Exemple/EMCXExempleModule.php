1bc8bdd28bb5f74140184488af08a720accf6762
Asuniia <43389096+Asuniia@users.noreply.github.com>
2023-04-09 15:46:16 +0200

    new module format

--- modules/Exemple/EMCXExempleModule.php
+++ modules/Exemple/EMCXExempleModule.php
@@ -1,15 +1,53 @@
 <?php
 
 namespace App\EMCX\modules\Exemple;
+
+use App\EMCX\EMCXLoader;
+use App\EMCX\src\Loader\EMCXModuleInterface;
 use ClientX\Module;
 use ClientX\Router;
 
-class EMCXExempleModule extends Module
+class EMCXExempleModule extends Module implements EMCXModuleInterface
 {
-    public function __construct(Router $route)
+
+    public function getName(): string
     {
-        $route->get("/exemple", function () {
-            return "Propulsé par EMCX pour la communauté ClientX !";
+        return "Exemple";
+    }
+
+    public function getVersion(): int
+    {
+        return 1;
+    }
+
+    public function getDescription(): string
+    {
+        return "Exemple de module";
+    }
+
+    public function getAuthor(): string
+    {
+        return "EMCX";
+    }
+
+    public function getEnabled(): bool
+    {
+        return true;
+    }
+
+    public Router $router;
+
+    public function __construct(EMCXLoader $emcx)
+    {
+        $this->router = $emcx->getContainer(Router::class);
+        $this->loadRoutes();
+    }
+
+    public function loadRoutes()
+    {
+        $this->router->get("/exemple", function () {
+            return "Exemple";
         }, "exemple");
     }
+
 }
